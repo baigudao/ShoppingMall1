72e269c9fbc70dee981dd8b0d21f9f646bab858b
jackie <971060378@qq.com>
2017-02-07 20:01:51 +0800

    实现主页面的布局

--- app/src/main/java/com/jackie/shoppingmall1/home/fragment/HomeFragment.java
+++ app/src/main/java/com/jackie/shoppingmall1/home/fragment/HomeFragment.java
@@ -1,33 +1,41 @@
 package com.jackie.shoppingmall1.home.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jackie.shoppingmall1.R;
 import com.jackie.shoppingmall1.base.BaseFragment;
 
 /**
  * Created by jackie on 2017/2/6 23:57.
  * QQ : 971060378
- * Used as : xxx
+ * Used as : 主页面的Fragment
  */
 public class HomeFragment extends BaseFragment {
 
-    private TextView textView;
+    private RecyclerView rvHome;
+    private ImageView ib_top;
+//    private HomeRecyclerAdapter adapter;
+    private TextView tv_search_home;
+    private TextView tv_message_home;
 
     @Override
     protected View initView() {
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(18);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        tv_search_home = (TextView) view.findViewById(R.id.tv_search_home);
+        tv_message_home = (TextView) view.findViewById(R.id.tv_message_home);
+        rvHome = (RecyclerView) view.findViewById(R.id.rv_home);
+        ib_top = (ImageView) view.findViewById(R.id.ib_top);
+        return view;
     }
 
+    /**
+     * 绑定数据
+     */
     @Override
     protected void initData() {
         super.initData();
-        textView.setText("主页内容");
     }
 }
